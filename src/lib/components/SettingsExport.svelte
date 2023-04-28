70d482bff25581508c82d3b522f19f9379e1bc77
Neyl66 <neyl.mage@seznam.cz>
2023-04-28 06:50:29 +0200

    🐛 Fix copying Settings in OBS browser

--- src/lib/components/SettingsExport.svelte
+++ src/lib/components/SettingsExport.svelte
@@ -1,6 +1,4 @@
 <script>
-    import {copy} from "svelte-copy";
-
     import {settings_db, usernames_db, users_blacklist_db, users_whitelist_db, users_aliases_db, users_tts_languages_db} from "../db.js";
 
     const all_data = {
@@ -11,20 +9,28 @@
         users_aliases: users_aliases_db.data,
         users_tts_languages: users_tts_languages_db.data,
     };
-    const stringified_data = JSON.stringify(all_data);
 
     let button_el;
+    let textarea_el;
 
-    function set_tooltip(text) {
-        button_el.setAttribute("data-tooltip", text);
+    function copy_text() {
+        textarea_el.select();
+        textarea_el.setSelectionRange(0, 99999);
+
+        // Clipboard API in OBS browser gives no permission to write text so only this works.
+        document.execCommand("copy");
+
+        // Add tooltip text.
+        button_el.setAttribute("data-tooltip", "Copied");
     }
+
 </script>
 
 <h3>Export settings</h3>
 
-<textarea readonly>{stringified_data}</textarea>
+<textarea bind:this={textarea_el} readonly>{JSON.stringify(all_data)}</textarea>
 
-<button bind:this={button_el} use:copy={stringified_data} on:svelte-copy={() => set_tooltip("Copied")} on:svelte-copy:error="{(event) => set_tooltip(`Failed to copy: ${event.detail.message}`)}">
+<button bind:this={button_el} on:click={copy_text}>
     Copy
 </button>
 
